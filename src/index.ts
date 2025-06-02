abee5146fd351a67d27c5fa26a538cce0e19933f
Karthik Mallya N <kars13m@gmail.com>
2025-06-02 15:32:42 +0530

    Updated Cors

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin : process.env.FRONTEND_URL || "http://localhost:3000",
     credentials : true
 }))
 app.use('/api/auth', Authrouter);
