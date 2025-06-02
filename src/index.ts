612cd05388ed204bd5e10fbb4e9d27eaba9ff11a
Karthik Mallya N <kars13m@gmail.com>
2025-06-02 15:37:28 +0530

    Updated Cors 2

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    credentials : true
-}))
+app.use(cors())
 app.use('/api/auth', Authrouter);
 app.use('/api/transactions', (req, res, next) => { authMiddle(req, res, next); }, TransRouter);
 
